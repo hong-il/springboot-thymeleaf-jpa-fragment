100c86b5aa1b676e59c04840c5782454e488575e
KIM HONGIL <clroyan01@gmail.com>
2019-11-09 04:44:43 +0900

    Create fragment project

---
+++ src/main/java/boot/jpa/fragment/FragmentApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.fragment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FragmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/boot/jpa/fragment/FragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.jpa.fragment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
